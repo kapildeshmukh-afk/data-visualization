bb0731bee62ec6935ce1edc3792b06611c263a06
kapildeshmukh-afk <80241134+kapildeshmukh-afk@users.noreply.github.com>
2021-10-12 23:18:52 +0530

    day3 ML assignemnt
    submitt

---
+++ day3/day3.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+import pandas as pd
+i
+mport numpy as np
+
+files=pd.read_csv('./titanic.csv')
+
+files.head(1000)
+
+
+print(files.isnull().sum())#there will be sum of errors 
+
+#to clean the error on cabin column
+ 
+files.Cabin=files.Cabin.fillna("null")
+
+#to clean the error on Age column
+ 
+files.Age=files.Age.fillna("age_empty")
+ 
+#to clean the error on FAre column
+ 
+files.Fare=files.Fare.fillna("fare_empty")
+
+ 
+print(files.isnull().sum())     
+#after this all error will be clean
+#graph for 50 range(split)
+sns.barplot(x='Fare',y='Pclass' ,data=files,bins=10 ,xLimit=50)
